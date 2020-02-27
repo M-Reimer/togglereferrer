1eb0f7c686d58fe2a0d51066467818741169275b
Manuel Reimer <manuel.reimer@gmx.de>
2020-02-27 15:52:20 +0100

    Unlock codepen.io again and add origin check for security

--- spoofing.js
+++ spoofing.js
@@ -1,6 +1,6 @@
 /*
     Firefox addon "Toggle Referrer"
-    Copyright (C) 2019  Manuel Reimer <manuel.reimer@gmx.de>
+    Copyright (C) 2020  Manuel Reimer <manuel.reimer@gmx.de>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -64,7 +64,8 @@ function CreateSpoofedReferrer(url, origin) {
 
     // "CodePen requires a referrer to render this..."
     case "s.codepen.io":
-      return "https://codepen.io/";
+    case "cdpn.io":
+      return (origin.host == "codepen.io") && "https://codepen.io/";
 
     // JSFiddle result is broken without referrer
     case "fiddle.jshell.net":
