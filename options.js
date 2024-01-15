a63f245c23b82c8ece42d46a6db7cd896a465c3f
Manuel Reimer <manuel.reimer@gmx.de>
2024-01-15 16:46:14 +0100

    Usage of <a> seems to be fixed for options pages. Remove workaround.

--- options.js
+++ options.js
@@ -43,17 +43,6 @@ function init() {
   loadOptions();
   checkSpoofing.addEventListener("change", checkSpoofingChanged);
   checkAutoDisable.addEventListener("change", CheckboxChanged);
-
-  // "Common web APIs" for opening new pages don't work here on Firefox for
-  // Android. So we hook onto the <a> elements and open the pages via
-  // WebExtensions API.
-  const links = document.getElementsByTagName('a');
-  for (let index = 0; index < links.length; index++) {
-    links[index].addEventListener("click", (event) => {
-      event.preventDefault();
-      browser.tabs.create({url: event.target.href});
-    });
-  }
 }
 
 function loadOptions() {
